f0bd98a37bf84cda09fa35df11829af8e34c0017
SarahElgindy <sarah.elgindy970@gmail.com>
2024-09-15 16:35:23 +0300

    Updated

--- python_introduction/basic_operations.py
+++ python_introduction/basic_operations.py
@@ -1,9 +1,9 @@
-number1 = 10
-number2 = 5
+number1 = 10 # amount of number 1
+number2 = 5 # amount of number 2
 
-Addition = number1 + number2
-Subraction = number1 - number2
-Multiplication = number1 * number2
+Addition = number1 + number2 # addition of number1 and number2
+Subraction = number1 - number2 # Subraction of number1 and number2
+Multiplication = number1 * number2 # Multiplication of number1 and number2
 
 print(f"Addition of {number1} and {number2} is {Addition}")
 print(f"Subraction of {number1} and {number2} is {Subraction}")
