234400728e1551396ae71b3b34919c14642342d9
SarahElgindy <sarah.elgindy970@gmail.com>
2024-09-29 21:38:33 +0300

    Updated

--- fns_and_dsa/shopping_list_manager.py
+++ fns_and_dsa/shopping_list_manager.py
@@ -12,7 +12,7 @@ def main():
 
     while True:
         display_menu()
-        choice = input("Enter your choice:")
+        choice = int(input("Enter your choice:"))
 
         if choice == '1':
             item = input("\nEnter the item to add:")
