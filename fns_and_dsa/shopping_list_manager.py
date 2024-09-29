4f303412bd8853be4f1dc42098f4c0a5cf6b1668
SarahElgindy <sarah.elgindy970@gmail.com>
2024-09-29 21:37:45 +0300

    Updated

--- fns_and_dsa/shopping_list_manager.py
+++ fns_and_dsa/shopping_list_manager.py
@@ -15,7 +15,7 @@ def main():
         choice = input("Enter your choice:")
 
         if choice == '1':
-            item = input("Enter the item to add:")
+            item = input("\nEnter the item to add:")
             if item:
                 shopping_list.append(item)
                 print(f"{item} has been added.")
