ac46ae5fa4507ba688786068ba8a45e69d2bf039
SarahElgindy <sarah.elgindy970@gmail.com>
2024-10-12 23:16:14 +0300

    updated

---
+++ oop/polymorphism_demo.py
@@ -0,0 +1,11 @@
+class Shape:
+    def area(self):
+        pass
+
+class Rectangle(Shape):
+    def __init__(self, lenght, width):
+        pass
+
+class Circle(Shape):
+    def __init__(self, radius):
+        pass
