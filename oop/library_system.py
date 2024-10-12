165d5e1352c7b864c1d1043a6b38a3d257f6f3de
SarahElgindy <sarah.elgindy970@gmail.com>
2024-10-12 23:07:48 +0300

    updated

--- oop/library_system.py
+++ oop/library_system.py
@@ -4,13 +4,18 @@ class Book:
     def __init__(self, title, author):
         self.title = title
         self.author = author
+
 class EBook(Book):
-    def __init__(self, file_size):
+    def __init__(self, title, author, file_size):
+        super().__init__(title, author)
         self.file_size = file_size
 class PrintBook(Book):
-    def __init__(self, page_count):
+    def __init__(self, title, author, page_count):
+        super().__init__(title, author)
         self.page_count = page_count
     
+
+
 class Library:
     def __init__(self):
         self.books = []  # Initialize an empty list to store books
