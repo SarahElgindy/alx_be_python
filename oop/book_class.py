a5335166d80c139c8f17b0654965654459d0fd7f
SarahElgindy <sarah.elgindy970@gmail.com>
2024-10-12 22:43:07 +0300

    Creating oop dir

---
+++ oop/book_class.py
@@ -0,0 +1,18 @@
+class Book:
+    def __init__(self, title, author, year):
+        self.title = title
+        self.author = author
+        self.year = year
+
+
+    def __del__(self):
+        print(f"Deleting {self.title} of the book")
+
+
+    def __str__(self):
+        return f"(title) by (author), published in (year)"
+    
+    def __repr__(self):
+        return f"Book('{self.title}', '{self.author}', {self.year})"
+
+
