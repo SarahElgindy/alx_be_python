4ab7d400841804e78028e22a3c8f07c30fe9e95e
SarahElgindy <sarah.elgindy970@gmail.com>
2024-10-12 23:02:25 +0300

    updated

--- oop/library_system.py
+++ oop/library_system.py
@@ -9,3 +9,13 @@ class PrintBook(Book):
     def __init__(self, page_count):
         self.page_count = page_count
     
+class Library:
+    def __init__(self):
+        self.books = []  # Initialize an empty list to store books
+
+    def add_book(self, book):
+        self.books.append(book)
+
+    def list_books(self):
+        for book in self.books:
+            print(book.get_details())
